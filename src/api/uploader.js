e8d712d9a417c13d998a3bf97f94267a67743ba2
Faraz <ifarazir@gmail.com>
2023-10-15 04:48:23 +0330

    change token

--- src/api/uploader.js
+++ src/api/uploader.js
@@ -14,7 +14,7 @@ const youzAxios = axios.create({
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'multipart/form-data',
-        'Authorization': 'Bearer b5113a3bd0d05005c7698d9439291cbb2cb03212273e4edc58e4fd33a0db437f73265a25e4537a334557f43f967c666c3978647832ffdd49746a59208c16e634fd7464de15c596f942bfbfad5e6b31dcc4ba3bdd552a80d65798e6b3f0a42907723c334f98b4a24b07f1f3425e390b03d5cc83cd7a3ebbf20bab6df9b3daefda'
+        'Authorization': 'Bearer b091bbe20100e7aac9718adcff533aa8f7c444a1cb91a1ca2ac1059485829c164d25592e5421dbabeba2c27ce160f9680af719b1e36d570e1449226508fd35b6556ffe0bc130e78a8cc9a1453ea070bf4fe437e58d59251dd143fff5ce3b545f1cdc405fb2e44b0be9977b2dcde848f620eee1cbd823271c7dfe0d99b1c5bdf3'
     }
 });
 
